d421d15bb70d7f95decf54b6d950f237305ecbbe
Coral <guscoraleski2006@gmail.com>
2026-03-04 20:33:25 -0300

    fazendo a estrutura do array bidimencional

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 void main() {
-    System.out.println("hello guys");
+
+    System.out.println("Welcome to Battle chip !");
+
+    int[][] matrizBi = new int[10][10];
+    System.out.println("Number coluna : ");
+
+    for(int i = 0; i < matrizBi.length; i++){
+        for(int j = 0; j < matrizBi[0].length; j++){
+            System.out.print(matrizBi[i][j] + " ");
+            matrizBi[2][3] = 2;
+        }
+        System.out.println();
+    }
+
 }
 
