764b43ab69f12585ce5aa26be90f3801ff2e97fd
Coral <guscoraleski2006@gmail.com>
2026-03-07 19:14:20 -0300

    fazendo a outra matriz.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
 void main() {
-
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Welcome to Battle chip !");
 
-    int[][] matrizBi = new int[10][10];
-    System.out.println("Number coluna : ");
+    String[][] matrizBi = new String[10][10];
+
+    System.out.println("Qual é a sua cordenada X ? :");
+    int cordenadaX = scanner.nextInt();
+
+    System.out.println("Qual é a sua cordenada Y ? :");
+    int cordenadaY = scanner.nextInt();
 
     for(int i = 0; i < matrizBi.length; i++){
         for(int j = 0; j < matrizBi[0].length; j++){
-            System.out.print(matrizBi[i][j] + " ");
-            matrizBi[2][3] = 2;
+            System.out.print(matrizBi[i][j] = "o" + " ");
+            matrizBi[cordenadaX][cordenadaY] = "X";
         }
         System.out.println();
     }
+
+    System.out.println("===================");
+
+    for(int i = 0; i < matrizBi.length; i++){
+        for(int j = 0; j < matrizBi[0].length; j++){
+            System.out.print(matrizBi[i][j] = "0" + " ");
+            matrizBi[cordenadaX][cordenadaY] = "X";
+        }
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
 
 }
 
